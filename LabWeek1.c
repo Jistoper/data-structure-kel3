b796765bfeeed0354dd7240b4b80a806af76e26b
Hunters565 <adrianagustinust@gmail.com>
2022-03-14 10:20:58 +0700

    Kerja kelompok 14 Mar 2022

--- LabWeek1.c
+++ LabWeek1.c
@@ -8,6 +8,13 @@
 
 void clear() { system("CLS"); }
 
+void delay(float number_of_seconds) {
+  float milli_seconds = 1000 * number_of_seconds;
+  clock_t start_time = clock();
+  while (clock() < start_time + milli_seconds)
+    ;
+}
+
 struct item {
   char code[6];
   char doll_name[30];
@@ -15,17 +22,61 @@ struct item {
   int price;
 };
 
-struct item arr_item[7];
+struct item arr_item[7] = {{.code = "DL003",
+                            .doll_name = "kangaroo Teddy Bear",
+                            .quantity = 25,
+                            .price = 60000},
+                           {.code = "DL005",
+                            .doll_name = "Lil Benny Phant",
+                            .quantity = 40,
+                            .price = 110000},
+                           {.code = "DL006",
+                            .doll_name = "Cute Baby Shark",
+                            .quantity = 29,
+                            .price = 130000},
+                           {.code = "DL012",
+                            .doll_name = "Plushie Cute Dinosaur",
+                            .quantity = 24,
+                            .price = 140000},
+                           {.code = "DL020",
+                            .doll_name = "Sweet Panda",
+                            .quantity = 28,
+                            .price = 150000},
+                           {.code = "DL029",
+                            .doll_name = "Reversible Octopus Plushie",
+                            .quantity = 18,
+                            .price = 150000},
+                           {.code = "DL033",
+                            .doll_name = "Cute Plush Cat Doll",
+                            .quantity = 18,
+                            .price = 200000}};
+
+void printarray() {
+  printf(
+      "No. | Code  |            Doll Name           | Available | Price        "
+      "\n");
+  for (int i = 0; i < 75; i++) printf("-");
+  printf("\n");
+  for (int i = 0; i < sizeof(arr_item) / sizeof(arr_item[0]); i++) {
+    printf("%.2d. | %.5s | %-30s | %-3.d       | Rp. %d,-\n", i + 1,
+           arr_item[i].code, arr_item[i].doll_name, arr_item[i].quantity,
+           arr_item[i].price);
+  }
+  for (int i = 0; i < 75; i++) printf("-");
+}
 
 void addstock() {
+  printf("Add Stock\n");
   char doll_code[5];
-  int i, marker = 0, quantity = 0;
+  int i, marker = 0, quantity = 0, temp = 0;
+
+  printarray();
 
   while (marker != 1) {
-    printf("\n\nInput Doll Code [ 5 chars ]: ");
-    scanf("%s", &doll_code);
+    printf("\nInput Doll Code [ 5 chars ]: ");
+    scanf("%s", doll_code);
 
-    for (i = 1; i <= 7; i++) {
+    for (i = 0; i < 7; i++) {
       if (strcmp(doll_code, arr_item[i].code) == 0) {
         marker = 1;
         break;
@@ -33,22 +84,38 @@ void addstock() {
     }
 
     if (marker != 1) {
+      printf("\n--The doll code doesn't exist--");
+      delay(2);
       clear();
-      printf("--The doll code doesn't exist--");
+      printf("Add Stock\n");
+      printarray();
     }
   }
 
-  clear();
-
   while (marker != 2) {
-    printf("\n\nInput Quantity [1...100]: ");
+    printf("Input Quantity [1...100]: ");
     scanf("%d", &quantity);
 
     if (quantity > 1 && quantity <= 100) {
       marker = 2;
     } else {
+      printf("\n--The quantity must be more than 0 and less than 101--");
+      delay(2);
       clear();
-      printf("--The quantity must be more than 0 and less than 101--");
+      printf("Add Stock\n");
+      printarray();
+      printf("\nInput Doll Code [ 5 chars ]: %s\n", doll_code);
+    }
+
+    temp = 999 - arr_item[i].quantity;
+    if (marker == 2 && quantity > temp) {
+      marker = 1;
+      printf("\n--Stock can't exceeds 999--");
+      delay(2);
+      clear();
+      printf("Add Stock\n");
+      printarray();
+      printf("\nInput Doll Code [ 5 chars ]: %s\n", doll_code);
     }
   }
   arr_item[i].quantity = arr_item[i].quantity + quantity;
@@ -56,35 +123,70 @@ void addstock() {
   printf("\n\n--Adding Stock Success--");
 }
 
-void sell() {
+int sell() {
   char code[6];
-  scanf("%s", code);
-}
-
-void printarray() {
-  printf(
-      "No. | Code  |           Doll Name          | Available | Price        "
-      "| ");
-  for (int i = 0; i < 5; i++) {
-    printf("%.2d | %.5s | %-30s |    %.3d    | Rp. %d", i + 1, arr_item[i].code,
-           arr_item[i].doll_name, arr_item[i].quantity, arr_item[i].price);
+  int track = -1;
+  int qnt;
+  while (1) {
+    track = -1;
+    clear();
+    printarray();
+    printf("\nInput doll code [5 chars]: ");
+    scanf("%s", code);
+    for (int i = 0; i < sizeof(arr_item) / sizeof(arr_item[0]); i++) {
+      if (strcmp(code, arr_item[i].code) == 0) {
+        track = i;
+        break;
+      }
+    }
+    if (track == -1) {
+      printf("--- The doll code doesn't exist ---\n");
+      delay(2);
+    } else
+      break;
   }
+
+  while (1) {
+    clear();
+    printarray();
+    printf("\nInput doll code [5 chars]: %s", code);
+    printf("\nInput quantity: ");
+    scanf("%d", &qnt);
+    if (qnt > arr_item[track].quantity) {
+      printf("...The quantity of doll is not enough...\n");
+      delay(2);
+      continue;
+    } else if (qnt < 1) {
+      printf("...Input invalid...\n");
+      delay(2);
+      continue;
+    }
+    break;
+  }
+  printf("total price is Rp. %d,- * %d = Rp. %d,-", arr_item[track].price, qnt,
+         arr_item[track].price * qnt);
+  arr_item[track].quantity -= qnt;
+  delay(5);
+  return 1;
 }
 
 int main() {
-  printarray();
+  // return 0;
   while (1) {
-    system("cls");
-    printf("1. Sell item\n2. Add stock\n3. Exit\n");
+    clear();
+    printarray();
+    printf("\n1. Sell item\n2. Add stock\n3. Exit\n");
+    printf("Input choice : ");
     int pil;
     scanf("%d", &pil);
-    system("cls");
+    clear();
     switch (pil) {
       case 1:
         sell();
         break;
 
       case 2:
+        clear();
         addstock();
         break;
 
